fe2da07b0a612c5d366f006f33b0dfc6cfc5b579
sridhar462 <sridhar462@gmail.com>
2025-07-29 15:02:31 +0200

    Handled Form Submission Using (ngSubmit), [(ngModel)]  and FormsModule

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-user-input',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-input.component.html',
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
 
+  initialInvestment = '0';
+  annualInvestment = '0';
+  expectedReturn = '5';
+  duration = '10';
+
+  submit() {
+    console.log("submitted the form");
+    console.log("initialInvestment", this.initialInvestment);
+    console.log("annualInvestment", this.annualInvestment);
+    console.log("expectedReturn", this.expectedReturn);
+    console.log("duration", this.duration);
+  }
 }
